dc6c02724c2976934281a082fd784feb641e5ce8
Abdulrahman Alfayad <aboudfayad@outlook.com>
2017-08-13 22:05:45 +0300

    fixed rules page (no more commas)

--- public/javascripts/rule/getRules.js
+++ public/javascripts/rule/getRules.js
@@ -7,11 +7,9 @@ $.get('/api/get-rules', function (rules, status) {
 	for (var i = 0; i < rules.length; i++) {
 		rules_container.innerHTML += '<div class="four wide column"><div class="ui card" id="r'+ i.toString() +'"></div></div>';
 		rule_container = document.getElementById(("r" + i.toString()));
-		console.log(rule_container);
-		rule_container.innerHTML = "\\[\\frac{"+ rules[i].premises.join("\\quad \\quad")+"}{"+ rules[i].conclusion +"}"+rules[i].rule+"\\]";
+		rules[i].premises = rules[i].premises[0].split(",");
+		rule_container.innerHTML = "\\[\\frac{"+ rules[i].premises.join(" \\quad \\quad ")+"}{"+ rules[i].conclusion +"}"+rules[i].rule+"\\]";
 		MathJax.Hub.Queue(["Typeset",MathJax.Hub,rule_container]);	
 	}
 
-	// for (var i = 0; i < rules.length; i++) {
-	// }
 });
