0fb120f551754dc33215227d8ad9d44c477e1265
Abdulrahman Alfayad <aboudfayad@outlook.com>
2018-06-20 13:49:04 +0300

    added function to get the symbol types from the table

--- linear-logic-translator/table.js
+++ linear-logic-translator/table.js
@@ -12,7 +12,7 @@ function table(symbols) {
 		table_body.innerHTML += '<tr><td id="t' + i.toString() + '" >$$'+  symbols[i] + '$$</td><td style="overflow: visible;"><select class="ui search fluid dropdown" style="z-index: 1; position: fixed" id="select-' + i +'" ><option value="">Type</option><option value="0">atom</option><option value="1">formula</option><option value="2">connective</option><option value="3">set</option><option value="4">unary</option><option value="5">primary separator</option><option value="6">separator</option></select></td></tr>';
 	}
 
-	table.innerHTML += '<a onClick="addRule()" class="ui teal button right floated" href="/">Translate</a>';
+	table.innerHTML += '<a onClick="getSymbolTypes()" class="ui teal button right floated">Translate</a>';
 
 	// rendering the symbols in mathjax
 	for (var i = 0; i < symbols.length; i++) {
@@ -20,4 +20,16 @@ function table(symbols) {
 	}
 	
 	$('.ui.search.dropdown').dropdown();
+}
+
+// Getting the symbol types from the table
+function getSymbolTypes() {
+	// getting the symbols
+	var symbols = getSequentSymbols()[1];
+	var symbol_types = [];
+
+	// getting the types
+	for (var i = 0; i < symbols.length; i++) {
+		symbol_types.push([symbols[i], $("#select-" + i).dropdown("get text")]);
+	}
 }
