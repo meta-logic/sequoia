700c3cd570774a8275de15866c04b8ad0c7ba049
Zan Naeem <znaeem@qatar.cmu.edu>
2019-07-04 15:21:57 +0300

    Made small change to GENFORM conn, in parser text

--- public/javascripts/rule/addRule.js
+++ public/javascripts/rule/addRule.js
@@ -49,7 +49,7 @@ BIFORM
 GENFORM
   = "(" _ form:FORM _ ")" { return form }
   / conn:CONN _ form:GENFORM { return "Form (" + conn + ", [" + form + "])" }
-  / conn:CONN _ "(" _ fls:FORM_LIST _ ")" {return 'Form(' + conn + "," + fls + ")"} 
+  / conn:CONN _ "(" _ fls:FORM_LIST _ ")" {return 'Form(' + conn + ",[" + fls.join(", ") + "])"}
   / form_var:FORM_VAR { return form_var }
   / atom_var:ATOM_VAR { return atom_var }
   / atom:ATOM { return atom }
