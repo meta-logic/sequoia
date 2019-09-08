bd5ce65625bdc6c7409cec3fe1841e66ad653809
Zan Naeem <znaeem@qatar.cmu.edu>
2019-09-08 14:43:02 +0300

    Changed ID manipulation to more sensible values

--- public/javascripts/apply/prooftree.js
+++ public/javascripts/apply/prooftree.js
@@ -1,5 +1,5 @@
-var rule_id = 1
-var rule_id_text = ""
+var leaf_id = "0"
+var seq_text = ""
 // A function to build on proof trees
 // Input:
 // - Proof tree html container ID (string)
@@ -7,20 +7,20 @@ var rule_id_text = ""
 // - Rule being applied (string)
 // - Array of derivations ([string])
 // Output: A branch/branches with the given derivation(s) expanding from the given branch in the proof tree provided
-function build_proof_tree(proof_tree_id, branch_id, rule, derivations) {
+function build_proof_tree(branch_id, rule, derivations) {
     // retrieving the proof tree branch html
-    var proof_tree = $("#" + proof_tree_id)[0]
+    var proof_tree = $("#prooftree")[0]
 
     // getting the total number of branches to assign ids for the new branches
     var branch_count = parseInt(proof_tree.getAttribute("count"))
-    proof_tree.setAttribute("count", branch_count + derivations.length)
+    proof_tree.setAttribute("count", branch_count + derivations.length - 1)
 
     // retrieving the proof tree branch html
-    var proof_tree_branch = $("#" + proof_tree_id + "_" + branch_id + " tbody")[0]
-    if (branch_count == 1) proof_tree_branch = $("#" + proof_tree_id + " tbody")[0]
+    var proof_tree_branch = $("#prooftree_" + branch_id + " tbody")[0]
+    if (branch_count == 1) proof_tree_branch = $("#prooftree tbody")[0]
 
     // creating the line on top of the branch
-    var conclusion = $("#" + proof_tree_id + "_" + branch_id + "_conc")[0]
+    var conclusion = $("#prooftree_" + branch_id + "_conc")[0]
     conclusion.setAttribute("class", "conc")
     conclusion.setAttribute("colspan", derivations.length)
 
@@ -32,14 +32,14 @@ function build_proof_tree(proof_tree_id, branch_id, rule, derivations) {
     for (var i = 0; i < derivations.length; i++) {
         new_branches +=
 			"<td><table id=\"" +
-			proof_tree_id +
+			"prooftree" +
 			"_" +
-			(branch_count + i + 1) +
+			branch_id + i +
 			"\"> \
             <tr><td class=\"conc-temp\" id=\"" +
-			proof_tree_id +
+			"prooftree" +
 			"_" +
-			(branch_count + i + 1) +
+			branch_id + i +
 			"_conc" +
 			"\">\\[" +
 			derivations[i] +
@@ -56,7 +56,7 @@ function build_proof_tree(proof_tree_id, branch_id, rule, derivations) {
     MathJax.Hub.Queue([ "Typeset", MathJax.Hub, proof_tree_branch.firstElementChild ])
 }
 
-$(".conc-temp").click(function() {
-    rule_id = parseInt(this.id.split("_")[1])
-    rule_id_text = $(this).find("script")[0].innerText
-})
+// $(".conc-temp").click(function() {
+//     leaf_id = parseInt(this.id.split("_")[1])
+//     seq_text = $(this).find("script")[0].innerText
+// })
