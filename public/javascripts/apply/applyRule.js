0a8030693bc250b733cb12d279eb3ed8fd423b15
Zan Naeem <znaeem@qatar.cmu.edu>
2019-09-08 14:41:37 +0300

    Added warnings and popup to choose premise

--- public/javascripts/apply/applyRule.js
+++ public/javascripts/apply/applyRule.js
@@ -1,33 +1,72 @@
-var counter = 1
 
 function applyRule(i) {
-    var rule = $("#r" + i).data("rule")
-    console.log(rule)
-    console.log(sequentDerivation)
+    var warning_text = "<div id=\"apply warning\"><div class=\"ui red negative message\">"+
+    "<div class=\"header\">Rule and Premise Mismatch</div>"+
+    "<p>This rule cannot be applied to the selected premise</p></div>"
 
-    sequentDerivation = parser.parse(rule_id_text).replace(/\\/g, "\\\\")
+    var ruletemp = document.getElementById("r"+i)
+    var name = ruletemp.getAttribute("rule_name")
+    var side = ruletemp.getAttribute("rule_name")[-1]
+    var conclusion = ruletemp.getAttribute("conclusion")
+    var premises = ruletemp.getAttribute("premises")
+    var rule_sml = "Rule(\""+name+"\",None,"+conclusion+","+premises+")"
+    var sequent = parser.parse(seq_text).replace(/\\/g, "\\\\")
+    var tree_sml = "DevTree(\""+leaf_id+"\","+sequent+", NoRule, [])"
 
-    $.post("/apply", { rule: rule, sequent: sequentDerivation }, function(data, status) {
-        console.log("Derivation: " + data.output.replace(/ "/g, "").replace(/" /g, "").replace(/"/g, ""))
-        console.log("Data: " + data + "\nStatus: " + status)
-        addDerivation(
-            rule,
-            data.output.replace(/ "/g, "").replace(/" /g, "").replace(/"/g, "").replace(/\\\\/g, "\\").split(",")
-        )
+    // console.log(rule_sml)
+    // console.log(tree_sml)
+
+    $.post("/apply", { rule: rule_sml, tree: tree_sml, node_id: "\""+leaf_id+"\"" }, function(data, status) {
+        var output = data.output.slice(1,-1).split("&&")
+        if (data.output == "NOT APPLICABLE"){
+            document.getElementById("warning").innerHTML = warning_text
+            return
+        }
+        var apply_warning = document.getElementById("apply warning")
+        if (apply_warning != null){
+            apply_warning.remove()
+        }
+        var prem_set = []
+        for (k = 0; k < output.length; k++) {
+            prem_set.push(output[k].trim().slice(1,-1).split("##"))
+        }
+
+        if (prem_set.length == 1) {
+            build_proof_tree(leaf_id, name, prem_set[0])
+            $(".conc-temp").click(function() {
+                leaf_id = this.id.split("_")[1]
+                seq_text = $(this).find("script")[0].innerText
+                console.log(seq_text)
+            })
+        }
+        else {
+            var index
+            var message = document.getElementById('info_prems')
+            message.innerHTML = 
+            "<div class=\"ui info message\">"+
+                "<div class=\"header\">Multiple Applications</div>"+
+                "<div class=\"content\">"+
+                    "<p>The rule can be applied to a the selected sequent in more than one way."+ 
+                    "Select the set of premises on how you would like to build the tree from the selected sequent.</p>"+
+                "</div><ul id = \"choice\" class=\"list\"></ul></div>"
+            
+            var choice = document.getElementById('choice')
+            for (i = 0; i < prem_set.length; i++) {
+                var display_prem = prem_set[i].join("\\qquad")
+                choice.innerHTML += "<div id = \"select\" num = "+i+" class=\"ui attahced secondary basic button select\">$$"+display_prem+"$$</div>"
+            }
+            MathJax.Hub.Queue([ "Typeset", MathJax.Hub, choice ])
+            $(".select").click(function() {
+                index = parseInt(this.getAttribute("num").toString())
+                message.innerHTML = "<br><br><br><br><br><br>"
+                build_proof_tree(leaf_id, name, prem_set[index])
+                $(".conc-temp").click(function() {
+                    leaf_id = this.id.split("_")[1]
+                    seq_text = $(this).find("script")[0].innerText
+                    console.log(seq_text)
+                })
+            })
+        }
     })
 }
 
-function addDerivation(rule, derivations) {
-    build_proof_tree("prooftree", rule_id, rule, derivations)
-    $(".conc-temp").click(function() {
-        rule_id = parseInt(this.id.split("_")[1])
-        rule_id_text = $(this).find("script")[0].innerText
-        console.log(rule_id_text)
-    })
-}
-
-function setSequent(i) {
-    console.log(i)
-    var seq = $("#" + i).data("rule").replace(/\(/g, "").replace(/\)/g, "")
-    sequentDerivation = parser.parse(seq).replace(/\\/g, "\\\\")
-}
