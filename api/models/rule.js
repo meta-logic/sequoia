825db3c0a61bb31b6510421e5fbbfb44dfb1964b
Zan Naeem <znaeem@qatar.cmu.edu>
2019-09-08 11:48:02 +0300

    Changed Extra arguments to sml translations

--- api/models/rule.js
+++ api/models/rule.js
@@ -4,14 +4,15 @@
 //Loading Dependencies =============================================
 var mongoose = require("mongoose")
 var Schema   = mongoose.Schema
-//var sequent  = require("./sequent")
+
 
 //defining the rule schema
 var ruleSchema = new Schema({
     rule       : String,
     premises   : [String],
     conclusion : String,
-    extraArguments : [String]
+    sml_prem   : [String],
+    sml_conc   : String
 })
 
 module.exports = mongoose.model("Rule", ruleSchema)
