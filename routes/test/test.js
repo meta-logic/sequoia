26ec6c3bf4e0d3d687b37db9d513767821df8757
Zan Naeem <znaeem@qatar.cmu.edu>
2019-09-25 09:30:46 +0300

    style changes and removed redundant items

--- routes/test/test.js
+++ routes/test/test.js
@@ -4,14 +4,14 @@
 //Loading Dependencies =============================================
 var express = require('express');
 var router  = express.Router();
-var sml     = require('../../sml/writeRule');
+// var sml     = require('../../sml/writeRule');
 
 router.get('/test', function (req, res) {
 	return res.render('test/test', {'title' : 'Sequoia - apply', 'layout' : 'test'});
 });
 
 router.post('/test', function (req, res) {
-	sml.writeRule(req.body.rule, req.body.conclusion, JSON.parse(req.body.premises), JSON.parse(req.body.conc), JSON.parse(req.body.prem));
+	// sml.writeRule(req.body.rule, req.body.conclusion, JSON.parse(req.body.premises), JSON.parse(req.body.conc), JSON.parse(req.body.prem));
 	return;
 });
 
