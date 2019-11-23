944b7c8aff08ab2800d46c9b2c2e8cfafc4aeb3b
Zan Naeem <znaeem@qatar.cmu.edu>
2019-11-23 22:13:37 +0300

    model user for future parts

---
+++ api/models/user.js
@@ -0,0 +1,16 @@
+//api/models/users.js
+"use strict"
+
+//Loading Dependencies =============================================
+var mongoose = require("mongoose")
+var Schema   = mongoose.Schema
+
+
+//defining the calculus schema
+var userSchema = new Schema({
+    username : String,
+    password : String, 
+    calculi  : [String]
+})
+
+module.exports = mongoose.model("User", userSchema)
