475c81b9f62697a00dc50ce10eebde296f180f8e
Zan Naeem <znaeem@qatar.cmu.edu>
2019-11-23 22:04:41 +0300

    Cleaned up the main routes and removed repeats

---
+++ routes/home.js
@@ -0,0 +1,49 @@
+//routes/main/home.js
+'use strict';
+
+//Loading Dependencies =============================================
+var express    = require('express');
+var router     = express.Router();
+var sml_apply        = require('../sml/applyRule');
+var sml_permute      = require('../sml/permuteRules');
+
+
+router.get('/', function (req, res) {
+	return res.render('main/index', {'title' : 'Sequoia','layout' : 'main'});
+});
+
+router.get('/calculus/:calc_id', function (req, res) {
+	return res.render('calculus/index', {'title' : 'Sequoia - calculus', 'layout' : 'calculus', 'calc_id' : req.params.calc_id});
+});
+
+router.get('/calculus/:calc_id/add-rule', function (req, res) {
+	return res.render('rule/index', {'title' : 'Sequoia - add rule', 'layout' : 'rule', 'calc_id' : req.params.calc_id,
+						'page' : 'add'});
+});
+
+router.get('/calculus/:calc_id/edit-rule/:rule_id', function (req, res) {
+	return res.render('rule/index', {'title' : 'Sequoia - edit rule', 'layout' : 'rule', 'calc_id' : req.params.calc_id, 
+					'rule_id' : req.params.rule_id, 'page' : 'edit'});
+});
+
+router.get('/calculus/:calc_id/apply', function (req, res) {
+	return res.render('apply/index', {'title' : 'Sequoia - apply', 'layout' : 'apply', 'calc_id' : req.params.calc_id});
+});
+
+router.post('/apply', function (req, res) {
+	var result = sml_apply.applyRule(req.body.rule, req.body.tree, req.body.node_id, res);
+});
+
+router.get('/calculus/:calc_id/properties', function (req, res) {
+	return res.render('properties/index', {'title' : 'Sequoia - properties', 'layout' : 'properties', 'calc_id' : req.params.calc_id});
+});
+
+router.get('/calculus/:calc_id/properties/permutability', function (req, res) {
+	return res.render('properties/permutability', {'title' : 'Sequoia - properties', 'layout' : 'permutability', 'calc_id' : req.params.calc_id});
+});
+
+router.post('/permute', function (req, res) {
+	var result = sml_permute.permuteRules(req.body.rule1, req.body.rule2, req.body.init_rules, res);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,35 +2,25 @@
 'use strict';
 
 //Loading Dependencies =============================================
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 3000;
-var helmet   = require('helmet');
-var path     = require('path');
-var mongoose = require('mongoose');
-var hbs      = require('express-handlebars');
-var home_routes    = require('./routes/main/home');
-var apply_routes   = require('./routes/main/apply');
-var calculusRoutes = require('./api/routes/calculus');
-var ruleRoutes     = require('./api/routes/rule');
-var symbolsRoutes  = require('./api/routes/symbols');
-// var treeRoutes     = require('./api/routes/prooftree');
-
-// testing
-// var test_routes   = require('./routes/test/test');
-
-
+var express      = require('express');
+var path         = require('path');
+var helmet       = require('helmet');
+var mongoose     = require('mongoose');
 var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
+var hbs          = require('express-handlebars');
+var cookieParser = require('cookie-parser');
+var app = express();
+var port = 3000;
 
 
 //loading local files ===============================================
-var database     = require('./config/db');
-var Rule         = require('./api/models/rule');
-var Symbols      = require('./api/models/symbols');
-var Calcs      = require('./api/models/calculus');
-// var ProofTree    = require('./api/models/prooftree');
+var home_routes    = require('./routes/home');
+// var apply_routes   = require('./routes/main/apply');
+var calculusRoutes = require('./api/routes/calculus');
+var ruleRoutes     = require('./api/routes/rule');
+var symbolsRoutes  = require('./api/routes/symbols');
+var database       = require('./config/db');
 
 
 //Configurations =====================================================
@@ -39,6 +29,7 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({extended: false})); // get information from html forms
+
 
 
 // view engine setup
@@ -58,66 +49,9 @@ mongoose.connect(database.local, {useNewUrlParser: true});
 app.use('/api', calculusRoutes);
 app.use('/api', ruleRoutes);
 app.use('/api', symbolsRoutes);
-// app.use('/api', treeRoutes);
-// app.use('/', test_routes);
 app.use('/', home_routes);
-app.use('/', apply_routes);
+// app.use('/', apply_routes);
 
-app.get('/api/get-rules', function (req, res) {
-	var rules = Rule.find({}, function (err, rules) {
-		if (err) {
-			console.log(err);
-		}
-		return res.json(rules);
-	});
-});
-
-app.get('/api/get-symbols', function (req, res) {
-	var symbols = Symbols.find({group : "rule"}, function (err, symbols) {
-		if (err) {
-			console.log(err);
-		}
-		return res.json(symbols);
-	});
-});
-
-app.get('/api/get-seq_symbols', function (req, res) {
-	var symbols = Symbols.find({group : "seq"}, function (err, symbols) {
-		if (err) {
-			console.log(err);
-		}
-		return res.json(symbols);
-	});
-});
-
-app.get('/api/get-certain_symbols', function (req, res) {
-	var symbols = Symbols.find({$or:[{group: "seq"},{type : {$in: ["connective", "sequent sign", "context separator", "empty"]}}]}, 
-	function (err, symbols) {
-		if (err) {
-			console.log(err);
-		}
-		return res.json(symbols);
-	});
-});
-
-app.get('/api/get-calculi', function (req, res) {
-	var calcs = Calcs.find({}, function (err, calcs) {
-		if (err) {
-			console.log(err);
-		}
-		return res.json(calcs);
-	});
-});
-
-// app.get('/api/get-prooftree', function (req, res) {
-// 	var tree = ProofTree.find({}, function (err, tree) {
-// 		if (err) {
-// 			console.log(err);
-// 		}
-// 		return res.json(tree);
-// 	});
-
-// });
 
 global.__basedir = __dirname;
 
