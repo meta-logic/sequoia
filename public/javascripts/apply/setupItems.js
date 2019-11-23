bfb5e82202418412ab28dbb460166e001ff5a278
Zan Naeem <znaeem@qatar.cmu.edu>
2019-11-23 22:24:06 +0300

    setups are now specific to calc

--- public/javascripts/apply/setupItems.js
+++ public/javascripts/apply/setupItems.js
@@ -1,1 +1,7 @@
-get_rules_toPage()
+var calc_id = document.getElementById("calc_id").innerHTML
+document.getElementById("Calculus icon").setAttribute("href", "/calculus/"+calc_id)
+document.getElementById("Proof Tree icon").setAttribute("href", "/calculus/"+calc_id+"/apply")
+document.getElementById("Properties icon").setAttribute("href", "/calculus/"+calc_id+"/properties")
+$.get("/api/calculus/"+calc_id, function (calc, status) {
+    get_rules_toPage()
+})
