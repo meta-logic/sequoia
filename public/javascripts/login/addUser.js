8cd5b8e76965629f9a4d0a74c874e607724fcf3d
Zan Naeem <znaeem@qatar.cmu.edu>
2019-12-02 19:40:18 +0300

    implmented registration

--- public/javascripts/login/addUser.js
+++ public/javascripts/login/addUser.js
@@ -1,9 +1,20 @@
 function addUser() {
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
-    if (username != "" && password != "") {
-        $.post("/api/user", { username : username, password : password}, function(data, status){
-            window.location.href = "/login"
-        }
-    )}
+    var email = document.getElementById("email").value
+    var occupation = document.getElementById("occupation").value
+    if (username != "" && password != "" && email != "" && occupation != "") {
+        $.get("/api/users/"+username, function (data, status) {
+            if (data.status != "success") {
+                $.post("/api/user", { username : username, password : password, 
+                    email : email, occupation : occupation}, function(data, status){
+                    window.location.href = "/login"
+                })
+            } else {
+                document.getElementById("warning").innerHTML =
+                "<div class=\"ui red negative message\">"+
+                "<p>Username already exists</p></div>"
+            }
+        })
+    }
 }
