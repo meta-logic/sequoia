784971942c34d26edffda4663b7a5757c51cfe3f
Zan Naeem <znaeem@qatar.cmu.edu>
2020-01-03 21:49:40 +0300

    made a way to update the parser with new terms

--- public/javascripts/apply/applyRule.js
+++ public/javascripts/apply/applyRule.js
@@ -1,4 +1,6 @@
 var constraint_history = []
+var rng_index = "0"
+var fresh_symbols = {}
 
 function applyRule(i) {
     var warning_text = "<div id=\"apply warning\"><div class=\"ui red negative message\">"+
@@ -17,55 +19,34 @@ function applyRule(i) {
     var conclusion = ruletemp.getAttribute("conclusion")
     var premises = ruletemp.getAttribute("premises")
     var rule_sml = "Rule(\""+name+"\",None,"+conclusion+","+premises+")"
-    //ADD THE NEW THINGS TO PARSER
-    var sequent = parser.parse(seq_text).replace(/\\/g, "\\\\")
-    var tree_sml = "DerTree(\""+leaf_id+"\","+sequent+", NoRule, [])"
-
-    $.post("/apply", { rule: rule_sml, tree: tree_sml, node_id: "\""+leaf_id+"\"" }, function(data, status) {
-        var output = data.output.slice(1,-1).split("&&")
-        if (data.output == "NOT APPLICABLE"){
-            document.getElementById("warning").innerHTML = warning_text
-            return
-        }
-        var apply_warning = document.getElementById("apply warning")
-        if (apply_warning != null){
-            apply_warning.remove()
-        }
-        var prem_set = []
-        var cons_set = []
-        for (k = 0; k < output.length; k++) {
-            prems_cons = output[k].split("@@")
-            prem_set.push(prems_cons[1].trim().slice(1,-1).split("##"))
-            cons_set.push(prems_cons[0].trim().slice(1,-1).split("##"))
-        }
-        if (prem_set.length == 1) {
-            build_proof_tree(leaf_id, name, prem_set[0])
-            $(".conc-temp").click(function() {
-                leaf_id = this.id.split("_")[1]
-                seq_text = $(this).find("script")[0].innerText
-                console.log(seq_text)
-            })
-        }
-        else {
-            var message = document.getElementById('info_prems')
-            message.innerHTML = 
-            "<div class=\"ui info message\">"+
-                "<div class=\"header\">Multiple Applications</div>"+
-                "<div class=\"content\">"+
-                    "<p>The rule can be applied to a the selected sequent in more than one way. "+ 
-                    "Select the set of premises for which you would like to use to continue building the tree on the selected sequent.</p>"+
-                "</div><ul id = \"choice\" class=\"list\"></ul></div>"
-            
-            var choice = document.getElementById('choice')
-            for (i = 0; i < prem_set.length; i++) {
-                var display_prem = prem_set[i].join("\\qquad ")
-                choice.innerHTML += "<div id = \"select\" num = "+i+" class=\"ui attahced secondary basic button select\">$$"+display_prem+"$$</div>"
+    updateParser(fresh_symbols, function() {
+        var sequent = parser.parse(seq_text).replace(/\\/g, "\\\\")
+        var tree_sml = "DerTree(\""+leaf_id+"\","+sequent+", NoRule, [])"
+        $.post("/apply", { rule: rule_sml, tree: tree_sml, node_id: "\""+leaf_id+"\"", index : rng_index }, function(data, status) {
+            var output = data.output.slice(1,-1).split("&&")
+            if (data.output == "NOT APPLICABLE"){
+                document.getElementById("warning").innerHTML = warning_text
+                return
             }
-            MathJax.Hub.Queue([ "Typeset", MathJax.Hub, choice ])
-            $(".select").click(function() {
-                index = parseInt(this.getAttribute("num").toString())
-                message.innerHTML = "<br><br><br><br><br><br>"
-                build_proof_tree(leaf_id, name, prem_set[index])
+            var apply_warning = document.getElementById("apply warning")
+            if (apply_warning != null){
+                apply_warning.remove()
+            }
+            var prem_set = []
+            var cons_set = []
+            for (k = 0; k < output.length; k++) {
+                prems_cons = output[k].split("@@")
+                cons_set.push(prems_cons[0].trim().slice(1,-1).split("##"))
+                prem_set.push(prems_cons[1].trim().slice(1,-1).split("##"))
+                fresh_list = prems_cons[2].trim().slice(1,-1).split("##")
+                for (var y = 0; y < fresh_list.length; y++) {
+                    fresh_symbols[fresh_list[y]] = fresh_list[y]
+                }
+                rng_index = prems_cons[3].trim().slice(1,-1).split("##")[0]
+            }
+            if (prem_set.length == 1) {
+                index = 0
+                build_proof_tree(leaf_id, name, prem_set[0])
                 $(".conc-temp").click(function() {
                     leaf_id = this.id.split("_")[1]
                     seq_text = $(this).find("script")[0].innerText
@@ -81,9 +62,47 @@ function applyRule(i) {
                     }
                 }
                 MathJax.Hub.Queue([ "Typeset", MathJax.Hub, constraints ])
-            })
-        }
+            }
+            else {
+                var message = document.getElementById('info_prems')
+                message.innerHTML = 
+                "<div class=\"ui info message\">"+
+                    "<div class=\"header\">Multiple Applications</div>"+
+                    "<div class=\"content\">"+
+                        "<p>The rule can be applied to a the selected sequent in more than one way. "+ 
+                        "Select the set of premises for which you would like to use to continue building the tree on the selected sequent.</p>"+
+                    "</div><ul id = \"choice\" class=\"list\"></ul></div>"
+                var choice = document.getElementById('choice')
+                for (i = 0; i < prem_set.length; i++) {
+                    var display_prem = prem_set[i].join("\\qquad ")
+                    choice.innerHTML += "<div id = \"select\" num = "+i+" class=\"ui attahced secondary basic button select\">$$"+display_prem+"$$</div>"
+                }
+                MathJax.Hub.Queue([ "Typeset", MathJax.Hub, choice ])
+
+                $(".select").click(function() {
+                    index = parseInt(this.getAttribute("num").toString())
+                    message.innerHTML = "<br><br><br><br><br><br>"
+                    build_proof_tree(leaf_id, name, prem_set[index])
+                    $(".conc-temp").click(function() {
+                        leaf_id = this.id.split("_")[1]
+                        seq_text = $(this).find("script")[0].innerText
+                        console.log(seq_text)
+                    })
+                    var constraints = document.getElementById('side_menu_L')
+                    constraint_history.push(cons_set[index])
+                    var the_constraints = constraint_history.flat()
+                    constraints.innerHTML = ""
+                    for (i = 0; i < the_constraints.length; i++) {
+                        if (the_constraints[i] != "") {
+                            constraints.innerHTML += '<div class="item">$$'+the_constraints[i]+'$$</div>'
+                        }
+                    }
+                    MathJax.Hub.Queue([ "Typeset", MathJax.Hub, constraints ])
+                })
+            }
+        })
     })
+    console.log(constraint_history)
 }
 
 
@@ -117,4 +136,5 @@ function undo() {
         }
         MathJax.Hub.Queue([ "Typeset", MathJax.Hub, constraints ])
     }
+    console.log(constraint_history)
 }
---
+++ public/javascripts/apply/updateParser.js
@@ -0,0 +1,68 @@
+var parser_copy = pt
+
+
+function updateParser(new_symbols, callback) {
+    var parser_text = parser_copy
+    var arrow = "SeqSign = \"NO-ARROW\" "
+    var sep = "CtxSep = \"NO-SEP\" "
+    var conn = "Conn = \"NO-Conn\" "
+    var set = "CtxVar = \"NO-SET\" "
+    var form = "FormVar = \"NO-FORM\" "
+    var atom_var = "AtomVar = \"NO-ATOMVAR\" "
+    var atom = "Atom = \"NO-ATOM\" "
+    var calc_id = document.getElementById("calc_id").innerHTML
+
+    var context_variables = []
+    for (var symbol in new_symbols) {
+        if (symbol.includes("\\")) {
+            symbol = "\\" + symbol
+        }
+        context_variables.push(symbol)
+    }
+    $.get("/api/cert_symbols/"+calc_id, function(sb, status) {
+        var syms = sb.symbols
+        syms = syms.sort(function(a, b){
+            return b.symbol.length - a.symbol.length
+        })
+        for (var i = 0; i < syms.length; i++) {
+            var symbol = syms[i].symbol
+            var type = syms[i].type
+            if (symbol.includes("\\")) {
+                symbol = "\\" + symbol
+            }
+            if (type == "sequent sign") {
+                arrow += "/ \"" + symbol + "\" "
+            }
+            else if (type == "context separator") {
+                sep += "/ \"" + symbol + "\" "
+            }
+            else if (type == "connective") {
+                conn += "/ \"" + symbol + "\" "
+            }
+            else if (type == "context variable") {
+                context_variables.push(symbol)
+            }
+            else if (type == "formula variable") {
+                form += "/ \"" + symbol + "\" "
+            }
+            else if (type == "atom variable") {
+                atom_var += "/ \"" + symbol + "\" "
+            }
+            else if (type == "atom") {
+                atom += "/ \"" + symbol + "\" "
+            }
+        }
+
+        context_variables = context_variables.sort(function(a, b){
+            return b.length - a.length
+        })
+        for (var i = 0; i < context_variables.length; i ++) {
+            set += "/ \"" + context_variables[i] + "\" "
+        }
+    extra_text = "\n" + arrow + "\n" + sep + "\n" + conn + "\n" + set + "\n" + form + "\n" + atom_var + "\n" + atom + "\n" 
+    parser_text += extra_text
+    parser = peg.generate(parser_text)
+    callback()
+    })
+}
+
