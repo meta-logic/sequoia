b3b32f21b4e3eb55fcf70e29b783f0a2790293bc
Zan Naeem <znaeem@qatar.cmu.edu>
2020-01-26 22:52:55 +0300

    added password confirmation

--- public/javascripts/login/addUser.js
+++ public/javascripts/login/addUser.js
@@ -1,14 +1,21 @@
 function addUser() {
     var username = document.getElementById("username").value
-    var password = document.getElementById("password").value
+    var password1 = document.getElementById("password1").value
+    var password2 = document.getElementById("password2").value
     var email = document.getElementById("email").value
     var occupation = document.getElementById("occupation").value
-    if (username != "" && password != "" && email != "" && occupation != "") {
+    if (username != "" && password1 != "" && email != "" && occupation != "") {
         $.get("/api/users/"+username, function (data, status) {
             if (data.status != "success") {
-                $.post("/api/user", { username : username, password : password, 
-                    email : email, occupation : occupation}, function(data, status){
-                    window.location.href = "/login"})
+                if (password1 == password2) {
+                    $.post("/api/user", { username : username, password : password, 
+                        email : email, occupation : occupation}, function(data, status){
+                        window.location.href = "/login"})
+                } else {
+                    document.getElementById("warning").innerHTML =
+                    "<div class=\"ui red negative message\">"
+                    +"<p>Passwords are not the same</p></div>"
+                }
             } else {
                 document.getElementById("warning").innerHTML =
                 "<div class=\"ui red negative message\">"
