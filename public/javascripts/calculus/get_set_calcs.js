b8a48133cea7fecd3193ef79d9b824a3226774ae
Zan Naeem <znaeem@qatar.cmu.edu>
2020-01-29 19:01:10 +0300

    Added LJ and LK sample calculi

--- public/javascripts/calculus/get_set_calcs.js
+++ public/javascripts/calculus/get_set_calcs.js
@@ -47,61 +47,10 @@ function addSomeCalculus(sample) {
     var user_id = document.getElementById("user_id").innerHTML
     var the_symbols = []
     var the_rules = []
-    $.post("/api/calculus", {title : "Sample Calculus", description : "This is a sample calculus with some basic rules. Try it out!", user : user_id}, function(data, status){
-        the_symbols = [ {symbol : "A", type: "formula variable", group : "rule", calculus : data.calculus._id},
-                        {symbol : "B", type: "formula variable", group : "rule", calculus : data.calculus._id},
-                        {symbol : "C", type: "formula variable", group : "rule", calculus : data.calculus._id},
-                        {symbol : "\\Gamma", type: "context variable", group : "rule", calculus : data.calculus._id},
-                        {symbol : "\\Gamma_1", type: "context variable", group : "rule", calculus : data.calculus._id},
-                        {symbol : "\\Gamma_2", type: "context variable", group : "rule", calculus : data.calculus._id},
-                        {symbol : "\\vdash", type: "sequent sign", group : "rule", calculus : data.calculus._id},
-                        {symbol : "\\wedge", type: "connective", group : "rule", calculus : data.calculus._id},
-                        {symbol : "\\vee", type: "connective", group : "rule", calculus : data.calculus._id},
-                        {symbol : "\\supset", type: "connective", group : "rule", calculus : data.calculus._id}
-                    ]
-        the_rules = [   {rule : "init", conclusion : '\\Gamma, A \\vdash A', 
-                            premises : JSON.stringify([]),
-                            parsed_conc : 'Seq (Single (Ctx ([CtxVar ("\\Gamma")], [FormVar ("A")])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("A")])))',
-                            parsed_prem : JSON.stringify([]), 
-                            calculus : data.calculus._id, connective : "", side : "None"},
-                        {rule : "\\wedge_L", conclusion : '\\Gamma, A \\wedge B \\vdash C', 
-                            premises : JSON.stringify(['\\Gamma, A, B \\vdash C']),
-                            parsed_conc : 'Seq (Single (Ctx ([CtxVar ("\\Gamma")], [Form (Con ("\\wedge"), [FormVar ("A"),FormVar ("B")])])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("C")])))',
-                            parsed_prem : JSON.stringify(['Seq (Single (Ctx ([CtxVar ("\\Gamma")], [FormVar ("A"), FormVar ("B")])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("C")])))']), 
-                            calculus : data.calculus._id, connective : "\\wedge", side : "Left"},
-                        {rule : "\\wedge_R", conclusion : '\\Gamma_1, \\Gamma_2 \\vdash A \\wedge B', 
-                            premises : JSON.stringify(['\\Gamma_1  \\vdash A', '\\Gamma_2  \\vdash B']),
-                            parsed_conc : 'Seq (Single (Ctx ([CtxVar ("\\Gamma_1"), CtxVar ("\\Gamma_2")], [])), Con ("\\vdash"), Single (Ctx ([], [Form (Con ("\\wedge"), [FormVar ("A"),FormVar ("B")])])))',
-                            parsed_prem : JSON.stringify(['Seq (Single (Ctx ([CtxVar ("\\Gamma_1")], [])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("A")])))', 'Seq (Single (Ctx ([CtxVar ("\\Gamma_2")], [])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("B")])))']), 
-                            calculus : data.calculus._id, connective : "\\wedge", side : "Right"},
-                        {rule : "\\vee_L", conclusion : '\\Gamma_1, \\Gamma_2, A \\vee B \\vdash C', 
-                            premises : JSON.stringify(['\\Gamma_1, A \\vdash C', '\\Gamma_2, B \\vdash C']),
-                            parsed_conc : 'Seq (Single (Ctx ([CtxVar ("\\Gamma_1"), CtxVar ("\\Gamma_2")], [Form (Con ("\\vee"), [FormVar ("A"),FormVar ("B")])])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("C")])))',
-                            parsed_prem : JSON.stringify(['Seq (Single (Ctx ([CtxVar ("\\Gamma_1")], [FormVar ("A")])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("C")])))', 'Seq (Single (Ctx ([CtxVar ("\\Gamma_2")], [FormVar ("B")])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("C")])))']), 
-                            calculus : data.calculus._id, connective : "\\vee", side : "Left"},
-                        {rule : "\\vee_{R_1}", conclusion : '\\Gamma \\vdash A \\vee B', 
-                            premises : JSON.stringify(['\\Gamma  \\vdash A']),
-                            parsed_conc : 'Seq (Single (Ctx ([CtxVar ("\\Gamma")], [])), Con ("\\vdash"), Single (Ctx ([], [Form (Con ("\\vee"), [FormVar ("A"),FormVar ("B")])])))',
-                            parsed_prem : JSON.stringify(['Seq (Single (Ctx ([CtxVar ("\\Gamma")], [])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("A")])))']), 
-                            calculus : data.calculus._id, connective : "\\vee", side : "Right"},
-                        {rule : "\\vee_{R_2}", conclusion : '\\Gamma \\vdash A \\vee B', 
-                            premises : JSON.stringify(['\\Gamma  \\vdash B']),
-                            parsed_conc : 'Seq (Single (Ctx ([CtxVar ("\\Gamma")], [])), Con ("\\vdash"), Single (Ctx ([], [Form (Con ("\\vee"), [FormVar ("A"),FormVar ("B")])])))',
-                            parsed_prem : JSON.stringify(['Seq (Single (Ctx ([CtxVar ("\\Gamma")], [])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("B")])))']), 
-                            calculus : data.calculus._id, connective : "\\vee", side : "Right"},
-                        {rule : "\\supset_L", conclusion : '\\Gamma_1, \\Gamma_2, A \\supset B \\vdash C', 
-                            premises : JSON.stringify(['\\Gamma_1 \\vdash A','\\Gamma_2, B \\vdash C']),
-                            parsed_conc : 'Seq (Single (Ctx ([CtxVar ("\\Gamma_1"), CtxVar ("\\Gamma_2")], [Form (Con ("\\supset"), [FormVar ("A"),FormVar ("B")])])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("C")])))',
-                            parsed_prem : JSON.stringify(['Seq (Single (Ctx ([CtxVar ("\\Gamma_1")], [])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("A")])))', 'Seq (Single (Ctx ([CtxVar ("\\Gamma_2")], [FormVar ("B")])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("C")])))']), 
-                            calculus : data.calculus._id, connective : "\\supset", side : "Left"},
-                        {rule : "\\supset_R", conclusion : '\\Gamma \\vdash A \\supset B', 
-                            premises : JSON.stringify(['\\Gamma, A \\vdash B']),
-                            parsed_conc : 'Seq (Single (Ctx ([CtxVar ("\\Gamma")], [])), Con ("\\vdash"), Single (Ctx ([], [Form (Con ("\\supset"), [FormVar ("A"),FormVar ("B")])])))',
-                            parsed_prem : JSON.stringify(['Seq (Single (Ctx ([CtxVar ("\\Gamma")], [FormVar ("A")])), Con ("\\vdash"), Single (Ctx ([], [FormVar ("B")])))']), 
-                            calculus : data.calculus._id, connective : "\\supset", side : "Right"}
-                    ]
-        $.post("/api/symbols_init", {items : JSON.stringify(the_symbols)}, function(data, status){
-            $.post("/api/rules_init", {items : JSON.stringify(the_rules)}, function(data, status){
+    $.post("/api/calculus", {title : sample, description : "This is a sample calculus with some basic rules. Try it out!", user : user_id}, function(data, status){
+        syms_rules = sample_calc(sample, data.calculus)
+        $.post("/api/symbols_init", {items : JSON.stringify(syms_rules[0])}, function(data, status){
+            $.post("/api/rules_init", {items : JSON.stringify(syms_rules[1])}, function(data, status){
                 document.getElementById("title").value = ""
                 document.getElementById("description").value = ""
                 get_calculi_toPage()
