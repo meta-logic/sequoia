a6f6e96e357b379f79eed378b62b54a85efce211
Zan Naeem <znaeem@qatar.cmu.edu>
2020-02-06 13:13:03 +0300

    fixed rule application model, and styling

--- public/javascripts/apply/applyRule.js
+++ public/javascripts/apply/applyRule.js
@@ -4,78 +4,62 @@ var fresh_symbols = {}
 var cut_var = ""
 var cut_form = ""
 
-
 function cutSelect (side, callback) {
-    document.getElementById("modal_warning").innerHTML = ""
-    var warning_text_form = "<div id=\"form warning\"><div class=\"ui red negative message\">"+
-    "<div class=\"header\">Formula Parsing Error</div>"+
-    "<p>Formulas must contain term symbols from the sequent symbols table</p></div>"
+    $("#modal_warning").css("visibility","hidden")
     if (side != "Cut") {
         callback()
     } else {
         $('#modal3').modal({
             onApprove: function () {
-                var v = document.getElementById("var").value
-                var f = document.getElementById("form").value
+                var v = $("#var").val()
+                var f = $("#form").val()
                 if (v == "" || f == "") {
-                    document.getElementById("modal_warning").innerHTML = warning_text_form
+                    $("#modal_warning").css("visibility","visible")
                     return false
                 } else {
                     cut_var = v
                     cut_form = f
-                    document.getElementById("var").value = ""
-                    document.getElementById("form").value = ""
+                    $("#var").val("")
+                    $("#form").val("")
                     callback()
                 }
             }
         })
-        .modal('setting', 'closable', false)
-        .modal('show')
+        .modal('setting', 'closable', false).modal('show')
     }
 }
 
 
 function applyRule(i) {
-    var warning_text_apply = "<div id=\"apply warning\"><div class=\"ui red negative message\">"+
-    "<div class=\"header\">Rule and Premise Mismatch</div>"+
-    "<p>This rule cannot be applied to the selected premise</p></div>"
-    var warning_text_seq = "<div id=\"seq warning\"><div class=\"ui red negative message\">"+
-    "<div class=\"header\">Sequent Parsing Error</div>"+
-    "<p>Sequents must be structurally valid and contain term symbols from the sequent symbols table</p></div>"
-    var warning_text_form = "<div id=\"form warning\"><div class=\"ui red negative message\">"+
-    "<div class=\"header\">Formula Parsing Error</div>"+
-    "<p>Formulas must contain term symbols from the sequent symbols table</p></div>"
-
-    if (leaf_id == "-1") {
-        return
-    }
-    var type = document.getElementById("prooftree_" + leaf_id + "_conc").getAttribute("class")
-    if (type == "conc") {
-        return
-    }
-    var ruletemp = document.getElementById("r"+i)
-    var name = ruletemp.getAttribute("rule_name").replace(/\\/g, "\\\\")
-    var conclusion = ruletemp.getAttribute("conclusion")
-    var premises = ruletemp.getAttribute("premises")
-    var side = ruletemp.getAttribute("side")
+    $("#warning").css("visibility","hidden")
+    $("#info").css("display","none")
+    if (leaf_id == "-1") return
+    if ($("#conc_"+leaf_id).attr("class") == "conclusion") return
+    var ruletemp = $("#r"+i)
+    var name = ruletemp.attr("rule_name").replace(/\\/g, "\\\\")
+    var side = ruletemp.attr("side")
+    var conclusion = ruletemp.attr("conclusion")
+    var premises = ruletemp.attr("premises")
     cutSelect (side, function () { 
         updateParser(fresh_symbols, function() {
             try {
                 var sequent = parser.parse(seq_text).replace(/\\/g, "\\\\")
             } catch(error) {
-                document.getElementById("warning").innerHTML = warning_text_seq
+                $("#warning_header").html("Sequent Parsing Error")
+                $("#warning_text").html("Sequents must be structurally valid and contain term symbols from the sequent symbols table.")
+                $("#warning").css("visibility","visible")
                 return
             }
             var rule_sml = "Rule(\""+name+"\",None,"+conclusion+","+premises+")"
             var tree_sml = "DerTree(\""+leaf_id+"\","+sequent+", NoRule, [])"
-            console.log("Rule : ",rule_sml)
-            console.log("Sequent : ",sequent)
-            params = { rule: rule_sml, tree: tree_sml, node_id: "\""+leaf_id+"\"", index : rng_index, subs: "[]" }
+            var params = { rule: rule_sml, tree: tree_sml, node_id: "\""+leaf_id+"\"", index : rng_index, subs: "[]" }
             if (side == "Cut") {
                 try {
                     var cF = formula_parser.parse(cut_form).replace(/\\/g, "\\\\")
                 } catch(error) {
-                    document.getElementById("warning").innerHTML = warning_text_form
+                    $("#warning_header").html("Formula Parsing Error")
+                    $("#warning_text").html("Formulas must contain term symbols from the sequent symbols table.")
+                    $("#warning").css("visibility","visible")
                     return
                 }
                 params = { rule: rule_sml, tree: tree_sml, node_id: "\""+leaf_id+"\"", index : rng_index, subs: "[Fs(FormVar(\""+cut_var+"\"),"+cF+")]" }
@@ -85,13 +69,15 @@ function applyRule(i) {
                 cut_form = ""
                 var output = data.output.slice(1,-1).split("&&")
                 if (data.output == "NOT APPLICABLE") {
-                    document.getElementById("warning").innerHTML = warning_text_apply
+                    $("#warning_header").html("Rule and Premise Mismatch")
+                    $("#warning_text").html("This rule cannot be applied to the selected premise.")
+                    $("#warning").css("visibility","visible")
                     return
                 }
                 var prem_set = []
                 var cons_set = []
-                for (k = 0; k < output.length; k++) {
-                    prems_cons = output[k].split("@@")
+                for (var k = 0; k < output.length; k++) {
+                    var prems_cons = output[k].split("@@")
                     cons_set.push(prems_cons[0].trim().slice(1,-1).split("##"))
                     prem_set.push(prems_cons[1].trim().slice(1,-1).split("##"))
                     fresh_list = prems_cons[2].trim().slice(1,-1).split("##")
@@ -103,70 +89,53 @@ function applyRule(i) {
                     rng_index = prems_cons[3].trim().slice(1,-1).split("##")[0]
                 }
                 if (prem_set.length == 1) {
-                    index = 0
-                    build_proof_tree(leaf_id, name, prem_set[0])
-                    $(".conc-temp").click(function() {
+                    var index = 0
+                    build_proof_tree(leaf_id, name.replace(/\\\\/g,"\\"), prem_set[0])
+                    $(".leaf").click(function() {
                         leaf_id = this.id.split("_")[1]
                         seq_text = $(this).find("script")[0].innerText
-                        var apply_warning = document.getElementById("apply warning")
-                        if (apply_warning != null) {
-                            apply_warning.remove()
-                        }
-                        var seq_warning = document.getElementById("seq warning")
-                        if (seq_warning != null) {
-                            seq_warning.remove()
-                        }
+                        $("#warning").css("visibility","hidden")
                         console.log(seq_text)
                     })
-                    var constraints = document.getElementById('side_menu_L')
                     constraint_history.push(cons_set[index])
                     var the_constraints = constraint_history.flat()
-                    constraints.innerHTML = ""
+                    var constraints = $("#side_menu_L")
+                    constraints.html("")
                     for (i = 0; i < the_constraints.length; i++) {
                         if (the_constraints[i] != "") {
-                            constraints.innerHTML += '<div class="item">$$'+the_constraints[i]+'$$</div>'
+                            constraints.append('<div class="item">$$'+the_constraints[i]+'$$</div>') 
                         }
                     }
-                    MathJax.Hub.Queue([ "Typeset", MathJax.Hub, constraints ])
-                }
-                else {
-                    var message = document.getElementById('info_prems')
-                    message.innerHTML = 
-                    "<div class=\"ui info message\">"+
-                        "<div class=\"header\">Multiple Applications</div>"+
-                        "<div class=\"content\">"+
-                            "<p>The rule can be applied to a the selected sequent in more than one way. "+ 
-                            "Select the set of premises for which you would like to use to continue building the tree on the selected sequent.</p>"+
-                        "</div><ul id = \"choice\" class=\"list\"></ul></div>"
-                    var choice = document.getElementById('choice')
-                    for (i = 0; i < prem_set.length; i++) {
+                    MathJax.Hub.Queue(["Typeset",MathJax.Hub,constraints[0]])
+                } else {
+                    var choice = $("#choice")
+                    choice.html("")
+                    for (var i = 0; i < prem_set.length; i++) {
                         var display_prem = prem_set[i].join("\\qquad ")
-                        choice.innerHTML += "<div id = \"select\" num = "+i+" class=\"ui attahced secondary basic button select\">$$"+display_prem+"$$</div>"
+                        choice.append('<div id="select" num='+i+' class="ui attahced secondary basic button select">$$'+display_prem+'$$</div>')
                     }
-                    MathJax.Hub.Queue([ "Typeset", MathJax.Hub, choice ])
+                    MathJax.Hub.Queue([ "Typeset",MathJax.Hub,choice[0]])
+                    $("#info").css("display","block")
                     $(".select").click(function() {
-                        index = parseInt(this.getAttribute("num").toString())
-                        message.innerHTML = "<br><br><br><br><br><br>"
-                        build_proof_tree(leaf_id, name, prem_set[index])
-                        $(".conc-temp").click(function() {
+                        var index = parseInt(this.getAttribute("num"))
+                        $("#info").css("display","none")
+                        build_proof_tree(leaf_id, name.replace(/\\\\/g,"\\"), prem_set[index])
+                        $(".leaf").click(function() {
                             leaf_id = this.id.split("_")[1]
                             seq_text = $(this).find("script")[0].innerText
-                            var apply_warning = document.getElementById("apply warning")
-                            if (apply_warning != null) {
-                                apply_warning.remove()
-                            }
+                            $("#warning").css("visibility","hidden")
                             console.log(seq_text)
                         })
-                        var constraints = document.getElementById('side_menu_L')
                         constraint_history.push(cons_set[index])
                         var the_constraints = constraint_history.flat()
-                        constraints.innerHTML = ""
-                        for (i = 0; i < the_constraints.length; i++) {
+                        var constraints = $("#side_menu_L")
+                        constraints.html("")
+                        for (var i = 0; i < the_constraints.length; i++) {
                             if (the_constraints[i] != "") {
-                                constraints.innerHTML += '<div class="item">$$'+the_constraints[i]+'$$</div>'
+                                constraints.append('<div class="item">$$'+the_constraints[i]+'$$</div>') 
                             }
                         }
-                        MathJax.Hub.Queue([ "Typeset", MathJax.Hub, constraints ])
+                        MathJax.Hub.Queue(["Typeset",MathJax.Hub,constraints[0]])
                     })
                 }
             })
@@ -176,38 +145,30 @@ function applyRule(i) {
 
 
 function undo() {
+    $("#warning").css("visibility","hidden")
+    $("#info").css("display","none")
     if (previous.length != 0) {
         var undo_items = previous.shift()
-        var undo_id = undo_items[0] 
-        var undo_branch_count = undo_items[1] 
-        var undo_seq = undo_items[2]
-        var proof_tree = $("#prooftree_0")[0]
-        proof_tree.setAttribute("count", undo_branch_count)
-        var conclusion = $("#prooftree_" + undo_id + "_conc")[0]
-        conclusion.setAttribute("class", "conc-temp")
-        $(".conc-temp").click(function() {
+        leaf_id = undo_items[0] 
+        seq_text = undo_items[1]
+        $("#applied_"+leaf_id).remove()
+        $("#delete_"+leaf_id).remove()
+        $("#conc_"+leaf_id).attr("class", "leaf")
+        $("leaf").click(function() {
             leaf_id = this.id.split("_")[1]
             seq_text = $(this).find("script")[0].innerText
-            var apply_warning = document.getElementById("apply warning")
-            if (apply_warning != null) {
-                apply_warning.remove()
-            }
+            $("#warning").css("visibility","hidden")
             console.log(seq_text)
         })
-        conclusion.removeAttribute("colspan")
-        document.getElementById("delete_id"+undo_id).remove()
-        leaf_id = undo_id
-        seq_text = undo_seq
-
-        var constraints = document.getElementById('side_menu_L')
         constraint_history.pop()
         var the_constraints = constraint_history.flat()
-        constraints.innerHTML = ""
-        for (i = 0; i < the_constraints.length; i++) {
+        var constraints = $("#side_menu_L")
+        constraints.html("")
+        for (var i = 0; i < the_constraints.length; i++) {
             if (the_constraints[i] != "") {
-                constraints.innerHTML += '<div class="item">$$'+the_constraints[i]+'$$</div>'
+                constraints.append('<div class="item">$$'+the_constraints[i]+'$$</div>') 
             }
         }
-        MathJax.Hub.Queue([ "Typeset", MathJax.Hub, constraints ])
+        MathJax.Hub.Queue(["Typeset",MathJax.Hub,constraints[0]])
     }
 }
--- public/javascripts/apply/prooftree.js
+++ public/javascripts/apply/prooftree.js
@@ -2,51 +2,22 @@ var previous = []
 var leaf_id = "-1"
 var seq_text = ""
 
-// A function to build on proof trees
-// Input:
-// - Proof tree html container ID (string)
-// - Proof tree branch to expand on (int)
-// - Rule being applied (string)
-// - Array of derivations ([string])
-// Output: A branch/branches with the given derivation(s) expanding from the given branch in the proof tree provided
 function build_proof_tree(branch_id, rule, derivations) {
-    // retrieving the proof tree branch html
-    var proof_tree = $("#prooftree_0")[0]
-
-    // getting the total number of branches to assign ids for the new branches
-    var branch_count = parseInt(proof_tree.getAttribute("count"))
-    previous.unshift([branch_id,branch_count,seq_text])
-    proof_tree.setAttribute("count", branch_count + derivations.length - 1)
-
-    // retrieving the proof tree branch html
-    var proof_tree_branch = $("#prooftree_" + branch_id + " tbody")[0]
-    console.log(branch_id)
-    console.log(proof_tree_branch)
-
-    // creating the line on top of the branch
-    var conclusion = $("#prooftree_" + branch_id + "_conc")[0]
-    conclusion.setAttribute("class", "conc")
-    conclusion.setAttribute("colspan", derivations.length)
-    
-    // creating the rule
-    var rule = "<td class=\"rulename\" rowspan=\"2\">"+
-                "<div class=\"rulename\">\\[\\scriptsize{" + rule.replace(/\\\\/g, "\\") + "}\\]</div></td>"
-
-    // creating the new derivation branches
-    var new_branches = ""
+    var proof_tree = $("#prooftree_"+branch_id)
+    proof_tree.append('<div id="applied_'+branch_id+'" class="rule">$$\\scriptsize{'+rule+'}$$</div>')
+    var new_trees = ""
     for (var i = 0; i < derivations.length; i++) {
-        new_branches +=
-		"<td><table id=\"prooftree_"+branch_id+i+"\">"+
-            "<tr><td class=\"conc-temp\" id=\"prooftree_"+branch_id+i+"_conc\">"+
-                "\\["+derivations[i]+"\\]"+
-            "</td></tr>"+
-        "</table></td>"
+        new_trees +=
+            '<div id="prooftree_'+branch_id+i+'" class="tree">'+
+                '<div id="exp_'+branch_id+i+'" class="sequence">'+
+                    '<div id="conc_'+branch_id+i+'" class="leaf">$$'+derivations[i]+'$$</div>'+
+                '</div>'+
+            '</div>'
     }
-    var row = "<tr id = delete_id"+ branch_id +" >" + new_branches + rule + "</tr>"
-
-    // adding the derivations to the branch
-    proof_tree_branch.innerHTML = row + proof_tree_branch.innerHTML
-
-    //applying mathjax
-    MathJax.Hub.Queue([ "Typeset", MathJax.Hub, proof_tree_branch.firstElementChild ])
+    $("#conc_"+branch_id).attr("class", "conclusion")
+    var tree_branch = $("#exp_"+branch_id)
+    tree_branch.html('<div id="delete_'+branch_id+'" class="premises">'+new_trees+'</div>'+tree_branch.html() )
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub,$("#applied_"+branch_id)[0]])
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,$("#delete_"+branch_id)[0]])
+    previous.unshift([branch_id,seq_text])
 }
