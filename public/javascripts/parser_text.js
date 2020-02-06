20d1ff8da3705ff1e9209ab059aa5d162babf11f
Zan Naeem <znaeem@qatar.cmu.edu>
2020-02-06 13:12:26 +0300

    fixed parser text error

--- public/javascripts/parser_text.js
+++ public/javascripts/parser_text.js
@@ -2,11 +2,11 @@ var pt =
 `
 // Sequent
 SEQ 
-  = _ ctx1:CTX_STR _ seq_sign:SeqSign _ ctx2:CTX_STR _{ return "Seq (" + ctx1 + ', Con ("' + seq_sign + '"), ' + ctx2 + ")" }
-
+  = _ ctx1:CTX_STR _ seq_sign:SeqSign _ ctx2:CTX_STR _ { return "Seq (" + ctx1 + ', Con ("' + seq_sign + '"), ' + ctx2 + ")" }
+  
 // Context struct
 CTX_STR 
-  = ctx:CTX _ ctx_sep:CtxSep _ ctx_str:CTX_STR { return "Mult ( Con (" + ctx_sep + "), " + ctx + ", " + ctx_str + ")" }
+  = ctx:CTX _ ctx_sep:CtxSep _ ctx_str:CTX_STR { return 'Mult ( Con ("' + ctx_sep + '"), ' + ctx + ", " + ctx_str + ")" }
   / ctx:CTX { return "Single (" + ctx + ")" } 
 
 // Context
@@ -49,7 +49,7 @@ BIFORM
 GENFORM
   = "(" _ form:FORM _ ")" { return form }
   / conn:CONN _ form:GENFORM { return "Form (" + conn + ", [" + form + "])" }
-  / conn:CONN _ "(" _ fls:FORM_LIST _ ")" {return 'Form(' + conn + ",[" + fls.join(", ") + "])"}
+  / conn:CONN _ "(" _ fls:FORM_LIST _ ")" {return "Form(" + conn + ",[" + fls.join(", ") + "])"}
   / form_var:FORM_VAR { return form_var }
   / atom_var:ATOM_VAR { return atom_var }
   / atom:ATOM { return atom }
