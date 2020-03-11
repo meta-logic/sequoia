452f980ec7f45fd0e2b3891883e9efe2430a1621
Zan Naeem <znaeem@qatar.cmu.edu>
2020-03-11 20:15:36 +0300

    added pointers to main menu for clarity

--- public/javascripts/calculus/setupItems.js
+++ public/javascripts/calculus/setupItems.js
@@ -1,2 +1,3 @@
 $("#main-menu").sticky({});
+$("#Home_icon").attr("class", "active item")
 get_calculi_toPage()
--- public/javascripts/rule/setupItems.js
+++ public/javascripts/rule/setupItems.js
@@ -1,4 +1,5 @@
 $("#main-menu").sticky({});
+$("#Calculus_icon").attr("class", "active item")
 var calc_id = $("#calc_id").text()
 $("#Calculus_icon").attr("href", "/calculus/"+calc_id)
 $("#Prooftree_icon").attr("href", "/calculus/"+calc_id+"/apply")
