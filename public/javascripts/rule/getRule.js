5ea2dd0b76fafb9ffd8aa4b36487f3f854ea98eb
Zan Naeem <znaeem@qatar.cmu.edu>
2020-04-08 15:46:17 +0300

    Added license info to the remaining files in cut

--- public/javascripts/rule/getRule.js
+++ public/javascripts/rule/getRule.js
@@ -1,3 +1,9 @@
+// Sequoia Copyright (C) 2020  Zan Naeem, Abdulrahman Alfayad
+// This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+// This is free software, and you are welcome to redistribute it
+// under certain conditions; type `show c' for details.
+
+
 $("#main-menu").sticky({});
 $("#Calculus_icon").attr("class", "active item")
 var calc_id = $("#calc_id").text()
